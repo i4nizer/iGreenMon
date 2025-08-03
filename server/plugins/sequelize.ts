3124fda54fd14375ae950f38ac88b2927201e089
i4nizer <iansandoval264@gmail.com>
2025-08-03 20:27:50 +0800

    Fixed deprecated sequelize logging.

--- server/plugins/sequelize.ts
+++ server/plugins/sequelize.ts
@@ -1,24 +1,28 @@
 import { Sequelize } from "sequelize"
-import { initModels, initModelRelationships } from "~~/server/services/sequelize"
+import {
+	initModels,
+	initModelRelationships,
+} from "~~/server/services/sequelize"
 
 //
 
 export default defineNitroPlugin(async () => {
-    // --- Instance and config
-    const config = useRuntimeConfig()
-    const isProd = config.nodeEnv == "production"
-    const sequelize = new Sequelize(config.databaseUrl, { logging: !isProd })
+	// --- Instance and config
+	const config = useRuntimeConfig()
+	const isProd = config.nodeEnv == "production"
+	const logging = isProd ? false : console.info
+	const sequelize = new Sequelize(config.databaseUrl, { logging })
 
-    // --- Run initializations
-    initModels(sequelize)
-    initModelRelationships()
+	// --- Run initializations
+	initModels(sequelize)
+	initModelRelationships()
 
-    // --- Authenticate
-    await sequelize.authenticate()
-    if (!isProd) console.info("Sequelize database authenticated.")
-    
-    // --- Sync on command
-    const syncDB = config.databaseSync === "sync"
-    if (syncDB) await sequelize.sync({ alter: true })
-    if (!isProd) console.info("Sequelize database tables synced.")
+	// --- Authenticate
+	await sequelize.authenticate()
+	if (!isProd) console.info("Sequelize database authenticated.")
+
+	// --- Sync on command
+	const syncDB = config.databaseSync === "sync"
+	if (syncDB) await sequelize.sync({ alter: true })
+	if (!isProd) console.info("Sequelize database tables synced.")
 })
