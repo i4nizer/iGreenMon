3a650ea00ff84dcdce3045002a0909ee68c1d651
i4nizer <iansandoval264@gmail.com>
2025-08-04 18:58:52 +0800

    Created a toast composable for notifications.

---
+++ app/composables/use-toast.ts
@@ -0,0 +1,39 @@
+
+//
+
+type ToastItem = {
+	text: string
+	color: string
+	timeout: number
+}
+
+//
+
+export const useToast = () => {
+	// --- Data
+	const toasts = useState<ToastItem[]>("toast")
+
+	// --- Loggers
+	const log = (text: string, color: string, timeout: number = 3000) => {
+		toasts.value.push({ text, color, timeout })
+	}
+
+    const info = (text: string, timeout: number = 3000) => {
+        toasts.value.push({ text, color: "info", timeout })
+    }
+    
+    const error = (text: string, timeout: number = 3000) => {
+        toasts.value.push({ text, color: "error", timeout })
+    }
+    
+    const warning = (text: string, timeout: number = 3000) => {
+        toasts.value.push({ text, color: "warning", timeout })
+    }
+
+    const success = (text: string, timeout: number = 3000) => {
+        toasts.value.push({ text, color: "success", timeout })
+    }
+
+    // --- Expose
+    return { toasts, log, info, error, warning, success }
+}
