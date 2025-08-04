0a9ecf6eae7defa26fb9f04c4b686b844f318996
i4nizer <iansandoval264@gmail.com>
2025-08-04 14:05:22 +0800

    Turned off path prefexing vue component names.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,12 @@ export default defineNuxtConfig({
 	compatibilityDate: "2025-07-15",
 	devtools: { enabled: false },
 	modules: ["vuetify-nuxt-module", "@vee-validate/nuxt"],
+	components: [
+		{
+			path: "~/components",
+			pathPrefix: false,
+		},
+	],
 	runtimeConfig: {
 		nodeEnv: "",
 		databaseUrl: "",
