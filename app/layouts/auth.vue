628d7ab5f371ef6941f7a10cbad51353ba6eb4c3
i4nizer <iansandoval264@gmail.com>
2025-08-04 18:50:35 +0800

    Added auth layout.

---
+++ app/layouts/auth.vue
@@ -0,0 +1,18 @@
+<template>
+    <v-layout full-height>
+        <v-app>
+            <!-- Contents -->
+            <v-main>
+                <slot></slot>
+            </v-main>
+        </v-app>
+    </v-layout>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
