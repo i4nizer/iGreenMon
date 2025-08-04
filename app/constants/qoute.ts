846b3677f97be258fc0dafb935d1795680a5a063
i4nizer <iansandoval264@gmail.com>
2025-08-04 18:13:46 +0800

    Added quotes for entertainment.

---
+++ app/constants/qoute.ts
@@ -0,0 +1,199 @@
+
+//
+
+const techQuotes = [
+	{
+		quote: "Your most unhappy customers are your greatest source of learning.",
+		quotee: "Bill Gates",
+	},
+	{
+		quote: "Move fast and break things. Unless you are breaking stuff, you are not moving fast enough.",
+		quotee: "Mark Zuckerberg",
+	},
+	{ quote: "Talk is cheap. Show me the code.", quotee: "Linus Torvalds" },
+	{
+		quote: "Programs must be written for people to read, and only incidentally for machines to execute.",
+		quotee: "Harold Abelson",
+	},
+	{
+		quote: "If you are born poor it's not your mistake, but if you die poor it's your mistake.",
+		quotee: "Bill Gates",
+	},
+	{
+		quote: "The question I ask myself like almost every day is, 'Am I doing the most important thing I could be doing?'",
+		quotee: "Mark Zuckerberg",
+	},
+	{ quote: "Stay hungry, stay foolish.", quotee: "Steve Jobs" },
+	{
+		quote: "The only way to do great work is to love what you do.",
+		quotee: "Steve Jobs",
+	},
+	{
+		quote: "First, solve the problem. Then, write the code.",
+		quotee: "John Johnson",
+	},
+	{
+		quote: "Code is like humor. When you have to explain it, it’s bad.",
+		quotee: "Cory House",
+	},
+	{
+		quote: "It’s not a faith in technology. It’s faith in people.",
+		quotee: "Steve Jobs",
+	},
+	{
+		quote: "Great software is not built by great programmers; it’s built by great teams.",
+		quotee: "Joel Spolsky",
+	},
+	{
+		quote: "Simplicity is the soul of efficiency.",
+		quotee: "Austin Freeman",
+	},
+	{
+		quote: "Don’t worry about failure; you only have to be right once.",
+		quotee: "Drew Houston",
+	},
+	{
+		quote: "It’s not about money. It’s about the people you have, how you’re led, and how much you get it.",
+		quotee: "Steve Jobs",
+	},
+	{
+		quote: "Good artists copy, great artists steal.",
+		quotee: "Steve Jobs (quoting Picasso)",
+	},
+	{
+		quote: "I choose a lazy person to do a hard job. Because a lazy person will find an easy way to do it.",
+		quotee: "Bill Gates",
+	},
+	{
+		quote: "Innovation distinguishes between a leader and a follower.",
+		quotee: "Steve Jobs",
+	},
+	{
+		quote: "You don’t learn to walk by following rules. You learn by doing, and by falling over.",
+		quotee: "Richard Branson",
+	},
+	{
+		quote: "If something is important enough, even if the odds are against you, you should still do it.",
+		quotee: "Elon Musk",
+	},
+	{
+		quote: "Any product that needs a manual to work is broken.",
+		quotee: "Elon Musk",
+	},
+	{
+		quote: "Persistence is very important. You should not give up unless you are forced to give up.",
+		quotee: "Elon Musk",
+	},
+	{
+		quote: "You have to be burning with an idea, or a problem, or a wrong that you want to right.",
+		quotee: "Steve Jobs",
+	},
+	{
+		quote: "The people who are crazy enough to think they can change the world are the ones who do.",
+		quotee: "Apple Inc.",
+	},
+	{
+		quote: "The most damaging phrase in the language is: ‘It’s always been done that way.’",
+		quotee: "Grace Hopper",
+	},
+	{
+		quote: "To handle yourself, use your head; to handle others, use your heart.",
+		quotee: "Eleanor Roosevelt",
+	},
+	{
+		quote: "Don't compare yourself with anyone in this world… if you do so, you are insulting yourself.",
+		quotee: "Bill Gates",
+	},
+	{
+		quote: "Sometimes it is the people no one can imagine anything of who do the things no one can imagine.",
+		quotee: "Alan Turing",
+	},
+	{
+		quote: "Computers are good at following instructions, but not at reading your mind.",
+		quotee: "Donald Knuth",
+	},
+	{
+		quote: "Software is a great combination between artistry and engineering.",
+		quotee: "Bill Gates",
+	},
+	{
+		quote: "A user interface is like a joke. If you have to explain it, it’s not that good.",
+		quotee: "Martin LeBlanc",
+	},
+	{
+		quote: "Before software can be reusable it first has to be usable.",
+		quotee: "Ralph Johnson",
+	},
+	{
+		quote: "You miss 100% of the shots you don’t take.",
+		quotee: "Wayne Gretzky (quoted by many tech leaders)",
+	},
+	{
+		quote: "The goal of a startup is to find a repeatable and scalable business model.",
+		quotee: "Steve Blank",
+	},
+	{
+		quote: "Your work is going to fill a large part of your life, and the only way to be truly satisfied is to do what you believe is great work.",
+		quotee: "Steve Jobs",
+	},
+	{
+		quote: "Ideas are easy. Implementation is hard.",
+		quotee: "Guy Kawasaki",
+	},
+	{
+		quote: "A good programmer is someone who always looks both ways before crossing a one-way street.",
+		quotee: "Doug Linder",
+	},
+	{
+		quote: "As a programmer, it is your job to put yourself out of business. What you do today can be automated tomorrow.",
+		quotee: "Doug McIlroy",
+	},
+	{
+		quote: "I’m not a great programmer; I’m just a good programmer with great habits.",
+		quotee: "Kent Beck",
+	},
+	{
+		quote: "There are two ways of constructing a software design. One way is to make it so simple that there are obviously no deficiencies.",
+		quotee: "C.A.R. Hoare",
+	},
+	{
+		quote: "The best way to predict the future is to invent it.",
+		quotee: "Alan Kay",
+	},
+	{
+		quote: "A computer is like a violin. You can imagine a novice trying first a phonograph and then a violin. The latter, he says, sounds terrible.",
+		quotee: "Marvin Minsky",
+	},
+	{
+		quote: "Programming is the art of algorithm design and the craft of debugging errant code.",
+		quotee: "Ellen Ullman",
+	},
+	{
+		quote: "It’s harder to read code than to write it.",
+		quotee: "Joel Spolsky",
+	},
+	{
+		quote: "Programming isn't about what you know; it’s about what you can figure out.",
+		quotee: "Chris Pine",
+	},
+	{
+		quote: "Experience is the name everyone gives to their mistakes.",
+		quotee: "Oscar Wilde (often quoted in dev talks)",
+	},
+	{
+		quote: "Machines take me by surprise with great frequency.",
+		quotee: "Alan Turing",
+	},
+	{
+		quote: "Learning to write programs stretches your mind and helps you think better.",
+		quotee: "Bill Gates",
+	},
+	{
+		quote: "Don’t get seduced by what you can do with computers. Do what matters.",
+		quotee: "Ethan Marcotte",
+	},
+] as const
+
+//
+
+export { techQuotes }
