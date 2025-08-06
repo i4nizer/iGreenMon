8336bdf164199265cde7c38ba33ae2e5aca57e5a
i4nizer <iansandoval264@gmail.com>
2025-08-06 22:08:58 +0800

    Added user verification in use-auth-verification composable.

--- app/composables/use-auth-verification.ts
+++ app/composables/use-auth-verification.ts
@@ -2,34 +2,51 @@
 //
 
 export const useAuthVerification = () => {
-
-    const getNextResendTime = async (
-        email: string
-    ): Promise<SafeResult<{ nextResendTime: number }>> => { 
-        try {
+	/** Provides the time when will the resend be available. */
+	const getNextResendTime = async (
+		email: string
+	): Promise<SafeResult<{ nextResendTime: number }>> => {
+		try {
 			const url = `/api/auth/verification/time`
 			const res = await $fetch(url, { method: "POST", body: { email } })
 			return { success: true, data: res }
 		} catch (error) {
-			const msg = (error as any)?.message ?? "Something went wrong."
+			const msg = (error as any)?.statusMessage ?? "Something went wrong."
 			return { success: false, error: msg }
 		}
-    }
+	}
 
 	/** Will fail if resend is on cooldown. */
 	const resendVerificationEmail = async (
 		email: string
-    ): Promise<SafeResult<{ nextResendTime: number }>> => {
-        try {
-            const url = `/api/auth/verification/resend`
+	): Promise<SafeResult<{ nextResendTime: number }>> => {
+		try {
+			const url = `/api/auth/verification/resend`
 			const res = await $fetch(url, { method: "POST", body: { email } })
+			return { success: true, data: res }
+		} catch (error) {
+			const msg = (error as any)?.statusMessage ?? "Something went wrong."
+			return { success: false, error: msg }
+		}
+	}
+
+	const verifyUser = async (
+		email: string,
+		token: string
+	): Promise<SafeResult<{ redirectUrl: string }>> => {
+		try {
+			const url = `/api/auth/verification/verify`
+			const res = await $fetch(url, {
+				method: "POST",
+				body: { email, token },
+            })
             return { success: true, data: res }
         } catch (error) {
-            const msg = (error as any)?.message ?? "Something went wrong."
+            const msg = (error as any)?.statusMessage ?? "Something went wrong."
 			return { success: false, error: msg }
         }
-    }
-    
-    // --- Expose
-    return { getNextResendTime, resendVerificationEmail }
+	}
+
+	// --- Expose
+	return { getNextResendTime, resendVerificationEmail, verifyUser }
 }
