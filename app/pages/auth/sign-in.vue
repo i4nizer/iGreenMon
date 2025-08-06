47fd7589eda69133af3dd580209f57fed9ca9d12
i4nizer <iansandoval264@gmail.com>
2025-08-07 00:19:09 +0800

    Created the sign-in page implementing the sign-in-form.

---
+++ app/pages/auth/sign-in.vue
@@ -0,0 +1,46 @@
+<template>
+	<v-container class="fill-height pa-0" fluid>
+		<v-row class="h-screen" justify="center">
+			<v-col cols="12" md="4" class="pa-0">
+				<quote-panel />
+			</v-col>
+			<v-col
+				cols="12"
+				md="8"
+				class="pa-0 d-flex align-center justify-center h-md-screen"
+			>
+				<div
+					class="w-100 w-md-75 w-xl-50 d-flex align-center justify-center"
+				>
+					<sign-in-form
+						@error="onSignInError"
+						@success="onSignInSuccess"
+					/>
+				</div>
+			</v-col>
+		</v-row>
+	</v-container>
+</template>
+
+<script setup lang="ts">
+
+//
+
+// --- Notifications
+const toast = useToast()
+
+// --- Redirect on success
+const onSignInSuccess = async (redirectUrl: string) => {
+	toast.success("User sign-in successful.")
+	await navigateTo(redirectUrl)
+}
+
+const onSignInError = async (msg: string) => {
+	toast.error(msg)
+}
+
+//
+
+</script>
+
+<style lang="css" scoped></style>
