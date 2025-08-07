75e6f00bfbc89895d38811ee4ad0acc9c7896f2f
i4nizer <iansandoval264@gmail.com>
2025-08-07 21:02:02 +0800

    Added middleware for verifying refresh token and attaching to context.

---
+++ server/middleware/02.refresh-meta.ts
@@ -0,0 +1,25 @@
+import { safeVerifyToken } from "~~/server/services/token"
+
+//
+
+/**
+ * This middleware verifies refresh token.
+ *  - if valid, it and its payload is attached to the context
+ *  - else, it is removed from cookies
+ */
+export default defineEventHandler(async (event) => {
+    // --- Skip non-user page requests
+    if (!event.context.isUserPage) return;
+
+    // --- Get valid refresh token
+    const refreshToken = getCookie(event, "refresh-token")
+    if (!refreshToken) return;
+    
+    // --- Verify refresh token
+    const verifyResult = safeVerifyToken(refreshToken, "Refresh")
+    if (!verifyResult.success) return deleteCookie(event, "refresh-token")
+
+    // --- Attach token and payload to event context
+    event.context.refreshToken = refreshToken
+    event.context.refreshTokenPayload = verifyResult.data
+})
