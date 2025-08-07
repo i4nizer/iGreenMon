84f76eb5979b42104efdca3296b996c59316a95d
i4nizer <iansandoval264@gmail.com>
2025-08-07 12:38:37 +0800

    Created the password changed page.

---
+++ app/pages/auth/recovery/email/[email]/changed.vue
@@ -0,0 +1,35 @@
+<template>
+    <v-container class="fill-height pa-0" fluid>
+        <v-row class="h-screen" justify="center">
+            <v-col cols="12" md="4" class="pa-0">
+				<quote-panel />
+			</v-col>
+			<v-col
+				cols="12"
+				md="8"
+				class="pa-0 d-flex align-center justify-center h-md-screen"
+			>
+				<div
+					class="w-100 w-md-75 w-xl-50 d-flex align-center justify-center"
+				>
+					<password-changed-card @redirect="onRedirect" />
+				</div>
+			</v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script setup lang="ts">
+
+//
+
+// --- Auto-redirect when countdown ends
+const onRedirect = async () => await navigateTo(`/auth/sign-in`)
+
+//
+
+</script>
+
+<style scoped>
+
+</style>
