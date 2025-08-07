9e451e82ca01de0a6e96d283fb6f46185a997a56
i4nizer <iansandoval264@gmail.com>
2025-08-07 21:03:47 +0800

    Added middleware for redirecting requests that is authorized already.

---
+++ server/middleware/04.path-redirection.ts
@@ -0,0 +1,36 @@
+import { User } from "~~/server/models/user"
+
+//
+
+/**
+ * This redirects user to user page when accessing
+ * auth page with access token, they must sign-out first.
+ */
+export default defineEventHandler(async (event) => {
+	// Note: If this is user route, access and refresh token will surely exists.
+	const { isApi, isAuthPage, accessToken, accessTokenPayload } = event.context
+
+	// --- Auth pages like /auth/sign-in shouldn't be accessed while signed-in
+	if (!isAuthPage || !accessToken) return
+
+	// --- Do not allow auth requests while signed-in to avoid anomaly
+	if (isApi) {
+		throw createError({
+			statusCode: 400,
+			statusMessage: "User must sign-out before accessing auth pages.",
+		})
+	}
+
+	// --- Determine who is signed-in
+	const id = accessTokenPayload.id as number
+	const user = await User.findByPk(id, { attributes: ["name"] })
+
+	// --- Rare case: dev anomaly
+	if (!user) {
+		deleteCookie(event, "access-token")
+		return deleteCookie(event, "refresh-token")
+	}
+
+	// --- Redirect to user page
+	return sendRedirect(event, `/user/${user.name}/greenhouse`)
+})
