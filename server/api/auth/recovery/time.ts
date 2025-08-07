a362cc7b68c27519444169d5beb0080f044ae4ac
i4nizer <iansandoval264@gmail.com>
2025-08-07 10:39:50 +0800

    Created api endpoint for checking cooldown time of allowed next forgot request.

---
+++ server/api/auth/recovery/time.ts
@@ -0,0 +1,29 @@
+import { UserEmailSchema } from "~~/shared/schema/user"
+import { getNextResetResendTime } from "~~/server/services/auth"
+
+//
+
+export default defineEventHandler(async (event) => {
+	// --- Validation
+	const body = await readBody(event)
+	const bodyResult = UserEmailSchema.safeParse(body)
+	if (!bodyResult.success) {
+		throw createError({
+			statusCode: 400,
+			statusMessage: bodyResult.error.message,
+		})
+	}
+
+	// --- Pass the logic to auth service
+	const { email } = bodyResult.data
+	const getTimeResult = await getNextResetResendTime(email)
+	if (!getTimeResult.success) {
+		throw createError({
+			statusCode: 400,
+			statusMessage: getTimeResult.error,
+		})
+	}
+
+	// --- Send the allowed next resend time
+	return { nextResendTime: getTimeResult.data.nextResendTime }
+})
