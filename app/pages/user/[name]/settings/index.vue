56a76d9b352e45244a4a46417c5679d1de0aacf0
i4nizer <iansandoval264@gmail.com>
2025-08-09 16:30:15 +0800

    Created the page for user settings.

---
+++ app/pages/user/[name]/settings/index.vue
@@ -0,0 +1,38 @@
+<template>
+	<v-container fluid class="bg-container pa-5 py-7">
+		<v-row>
+			<v-col>
+				<h3>User Settings</h3>
+			</v-col>
+		</v-row>
+        <v-row>
+            <v-col cols="12" md="6">
+                <v-card class="pa-5">
+                    <account-update-form 
+                        :user="(user as UserSafe)"
+                        @error="onUpdateUserError"
+                        @success="onUpdateUserSuccess"
+                    />
+                </v-card>
+            </v-col>
+        </v-row>
+	</v-container>
+</template>
+
+<script setup lang="ts">
+
+//
+
+// --- Notifs
+const toast = useToast()
+
+// --- User
+const { user } = useUser()
+const onUpdateUserError = (msg: string) => toast.error(msg)
+const onUpdateUserSuccess = () => toast.success("User updated successfully.")
+
+//
+
+</script>
+
+<style scoped></style>
