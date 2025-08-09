066a0c84f7cd80fa907cd1c454015071da07069f
i4nizer <iansandoval264@gmail.com>
2025-08-09 17:45:26 +0800

    Applied app's path redirection middleware in both ssr and client with correction to the signed-in user in the url.

--- app/middleware/00.path-redirection.global.ts
+++ app/middleware/00.path-redirection.global.ts
@@ -6,24 +6,47 @@
  * Redirects user to sign-in page when there is no access token. 
  */
 export default defineNuxtRouteMiddleware(async (to, from) => {
-    // --- Client-side only
-    if (!import.meta.client) return;
+    // --- Route notifs
+    const toast = useToast()
 
     // --- Get paths and tokens
     const isAuthPage = to.path.startsWith("/auth")
-    const isUserPage = /^\/user\/[^\/]+$/.test(to.path)
+    const isUserPage = to.path.startsWith("/user")
     const accessToken = useCookie("access-token")
 
-    // --- Trying to access user page without access token
-    if (isUserPage && !accessToken.value && to.path != "/auth/sign-in") {
-        return await navigateTo("/auth/sign-in")
+    // --- User page constraints that needs redirects
+    if (isUserPage) {
+        // --- Trying to access user page without access token
+        if (!accessToken.value && to.path != "/auth/sign-in") {
+            toast.info("Kindly sign-in first to access user pages.")
+            return await navigateTo("/auth/sign-in")
+        }
+
+        // --- Check who really is signed-in
+        const { whoami } = useUser({ hydrate: false })
+        const userResult = await whoami()
+        if (!userResult.success) {
+            return toast.error("Something went wrong, kindly refresh the page.")
+        }
+        
+        // --- Trying to access user page with different name
+        const user = userResult.data
+        const isNameMatch = user.name == to.params.name
+        if (!isNameMatch) {
+            // --- Replace the name segment
+            const segments = to.path.split("/")
+            segments[2] = user.name
+            return await navigateTo(segments.join("/"), { replace: true })
+        }
     }
     
     // --- Trying to access auth page with access token
     if (isAuthPage && accessToken.value && !to.path.startsWith("/user")) {
         const { whoami } = useUser({ hydrate: false })
         const userResult = await whoami()
-        if (!userResult.success) return;
+        if (!userResult.success) {
+            return toast.error("Something went wrong, kindly refresh the page.")
+        }
         
         const user = userResult.data
         return await navigateTo(`/user/${user.name}/greenhouse`)
