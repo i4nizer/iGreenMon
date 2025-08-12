a9b435a43cc41aeef5105996c5a607bbcc1f7a8e
i4nizer <iansandoval264@gmail.com>
2025-08-13 00:38:16 +0800

    Created a assoc joined type for invitation.

--- shared/types/invitation.ts
+++ shared/types/invitation.ts
@@ -1,14 +1,20 @@
 import type { z } from "zod"
+import type { User } from "#shared/types/user"
+import type { Greenhouse } from "#shared/types/greenhouse"
 import type {
-	InvitationCreateSchema,
 	InvitationSchema,
+	InvitationCreateSchema,
 } from "#shared/schema/invitation"
 
 //
 
 type Invitation = z.infer<typeof InvitationSchema>
+type InvitationGet = Invitation
+    & { invitee: Pick<User, "name"> }
+    & { inviter: Pick<User, "name"> }
+    & { greenhouse: Pick<Greenhouse, "name"> }
 type InvitationCreate = z.infer<typeof InvitationCreateSchema>
 
 //
 
-export type { Invitation, InvitationCreate }
+export type { Invitation, InvitationGet, InvitationCreate }
