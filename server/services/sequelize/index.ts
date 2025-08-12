f1dd09b540388764f7fb0cf81295f30ae6494fc8
i4nizer <iansandoval264@gmail.com>
2025-08-12 18:08:35 +0800

    Added Invitation in the database model associations.

--- server/services/sequelize/index.ts
+++ server/services/sequelize/index.ts
@@ -1,6 +1,7 @@
 import { User, userAttributes, userOptions } from "~~/server/models/user";
 import { Token, tokenAttributes, tokenOptions } from "~~/server/models/token";
 import { Greenhouse, greenhouseAttributes, greenhouseOptions } from "~~/server/models/greenhouse";
+import { Invitation, invitationAttributes, invitationOptions } from "~~/server/models/invitation";
 import { Sequelize } from "sequelize";
 
 //
@@ -11,6 +12,7 @@ const initModels = (sequelize: Sequelize) => {
 	User.init(userAttributes, userOptions(sequelize))
 	Token.init(tokenAttributes, tokenOptions(sequelize))
 	Greenhouse.init(greenhouseAttributes, greenhouseOptions(sequelize))
+	Invitation.init(invitationAttributes, invitationOptions(sequelize))
 }
 
 /** Binds model relationships. (hasMany, belongsTo) */
@@ -18,8 +20,16 @@ const initModelRelationships = () => {
 	// --- Model relationships
 	User.hasMany(Token, { foreignKey: "userId", onDelete: "CASCADE" })
 	User.hasMany(Greenhouse, { foreignKey: "userId", onDelete: "CASCADE" })
+	User.hasMany(Invitation, { foreignKey: "userId", onDelete: "CASCADE" })
+	
 	Token.belongsTo(User, { as: "user", foreignKey: "userId" })
+	
 	Greenhouse.belongsTo(User, { as: "user", foreignKey: "userId" })
+	Greenhouse.hasMany(Invitation, { foreignKey: "greenhouseId", onDelete: "CASCADE" })
+
+	Invitation.belongsTo(User, { as: "inviter", foreignKey: "inviterId" })
+	Invitation.belongsTo(User, { as: "invitee", foreignKey: "inviteeId" })
+	Invitation.belongsTo(Greenhouse, { as: "greenhouse", foreignKey: "greenhouseId" })
 }
 
 //
