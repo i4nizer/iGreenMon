940c4c44c62131eedb1c9d5de6fc9e8d916adec5
i4nizer <iansandoval264@gmail.com>
2025-08-15 01:06:51 +0800

    Created crew table for listing greenhouse's crews.

---
+++ app/components/user/greenhouse/crew/crew-table.vue
@@ -0,0 +1,37 @@
+<template>
+    <v-data-table
+        class="border"
+        :headers
+        :items="crews"
+    >
+        <template #item.user="{ item }">
+            {{ item.user.name }}
+        </template>
+        <template #item.createdAt="{ item }">
+            {{ date.format(item.createdAt, "fullDateTime12h") }}
+        </template>
+    </v-data-table>
+</template>
+
+<script setup lang="ts">
+
+//
+
+// --- Data binding
+const props = defineProps<{ crews: CrewGet[] }>()
+
+// --- Format
+const headers = [
+    { title: "Crew", value: "user" },
+    { title: "Date Joined", value: "createdAt" },
+]
+
+const date = useDate()
+
+//
+
+</script>
+
+<style scoped>
+
+</style>
