2de09b102bb19983ddd2e6a58fbabffc3a0b2503
i4nizer <iansandoval264@gmail.com>
2025-08-15 22:05:24 +0800

    Added remove button in crew tables.

--- app/components/user/greenhouse/crew/crew-table.vue
+++ app/components/user/greenhouse/crew/crew-table.vue
@@ -10,6 +10,15 @@
         <template #item.createdAt="{ item }">
             {{ date.format(item.createdAt, "fullDateTime12h") }}
         </template>
+        <template #item.actions="{ item }">
+            <v-btn
+                text="Remove"
+                color="red"
+                class="border"
+                elevation="0"
+                @click="emit('remove', item)"
+            ></v-btn>
+        </template>
     </v-data-table>
 </template>
 
@@ -18,12 +27,14 @@
 //
 
 // --- Data binding
+const emit = defineEmits<{ remove: [crew: CrewGet] }>()
 const props = defineProps<{ crews: CrewGet[] }>()
 
 // --- Format
 const headers = [
     { title: "Crew", value: "user" },
     { title: "Date Joined", value: "createdAt" },
+    { title: "Actions", value: "actions" },
 ]
 
 const date = useDate()
