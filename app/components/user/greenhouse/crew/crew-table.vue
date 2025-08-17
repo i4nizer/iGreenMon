66e31d7c5a0b0395be43c97ba93aab8970c88104
i4nizer <iansandoval264@gmail.com>
2025-08-17 22:56:00 +0800

    Added permissions column on crew table.

--- app/components/user/greenhouse/crew/crew-table.vue
+++ app/components/user/greenhouse/crew/crew-table.vue
@@ -1,43 +1,108 @@
 <template>
     <v-data-table
         class="border"
+        striped="even"
         :headers
-        :items="crews"
+        :items="crewsWithPermissions"
     >
         <template #item.user="{ item }">
             {{ item.user.name }}
         </template>
-        <template #item.createdAt="{ item }">
-            {{ date.format(item.createdAt, "fullDateTime12h") }}
+        <template #item.permissions="{ item }">
+            <div class="d-flex flex-wrap gx-1">
+                <v-chip
+                    v-for="perm in getPermissionCruds(item.permissions)"
+                    class="ma-1 bg-white"
+                    :key="perm"
+                    :text="perm"
+                    v-tooltip="getPermissionCrudTooltip(item, perm)"
+                ></v-chip>
+            </div>
         </template>
         <template #item.actions="{ item }">
             <v-btn
                 text="Remove"
                 color="red"
-                class="border"
                 elevation="0"
+                v-tooltip="`Remove Crew`"
                 @click="emit('remove', item)"
             ></v-btn>
+            <v-btn
+                text="Permission"
+                color="blue"
+                class="ml-1"
+                elevation="0"
+                v-tooltip="`Edit Permissions`"
+                @click="emit('permission', item)"
+            ></v-btn>
+        </template>
+        <template #item.createdAt="{ item }">
+            {{ date.format(item.createdAt, "fullDateTime12h") }}
         </template>
     </v-data-table>
 </template>
 
 <script setup lang="ts">
+import { PermissionType } from '~~/shared/schema/permission';
+
 
 //
 
 // --- Data binding
-const emit = defineEmits<{ remove: [crew: CrewGet] }>()
-const props = defineProps<{ crews: CrewGet[] }>()
+const emit = defineEmits<{
+    remove: [crew: CrewGet]
+    permission: [permission: CrewGet]
+}>()
+
+const props = defineProps<{
+    crews: CrewGet[]
+    permissions: Permission[]
+}>()
 
 // --- Format
+const date = useDate()
 const headers = [
     { title: "Crew", value: "user" },
-    { title: "Date Joined", value: "createdAt" },
+    { title: "Permissions", value: "permissions", width: "50%" },
     { title: "Actions", value: "actions" },
+    { title: "Date Joined", value: "createdAt" },
 ]
 
-const date = useDate()
+// --- Mapped Crew Permissions
+const crewsWithPermissions = computed(() =>
+    props.crews.map((c) => ({
+        ...c,
+        permissions: props.permissions.filter((p) => p.crewId == c.id),
+    }))
+)
+
+const getPermissionCruds = (permission: Permission[]) => {
+    const permKeys = permission.map((p) => `${p.resource}:${p.type}`)
+    const permKeySet = new Set(permKeys as PermissionKey[])
+    const permResources = permission.map((p) => p.resource)
+    const permResourceSet = new Set(permResources)
+
+    return [...permResourceSet].map((pr) => {
+        const affix = PermissionType
+            .filter((pt) => permKeySet.has(`${pr}:${pt}`))
+            .map((pt) => pt.at(0) as string)
+            .join("")
+        return `${pr}:${affix}`
+    }) as `${PermissionResource}:${string}`[]
+}
+
+const getPermissionCrudTooltip = (
+    crew: CrewGet,
+    permCrud: `${PermissionResource}:${string}`
+) => {
+    const resCrud = permCrud.split(":") as [PermissionResource, string]
+    const [resource, crud] = resCrud
+    const types = PermissionType
+        .filter((pt) => crud.includes(pt.at(0) as string))
+        .map((pt) => pt.toLowerCase())
+        .join(", ")
+    return `${crew.user.name} can ${types} your ${resource.toLowerCase()}s.`
+}
 
 //
 
