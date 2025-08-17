d7bc3de62e0ec223080b333f1dcba24d5971a7a4
i4nizer <iansandoval264@gmail.com>
2025-08-17 22:52:04 +0800

    Created permission service under greenhouse/crew to handler permission granting and revoking.

---
+++ server/services/greenhouse/crew/permission/index.ts
@@ -0,0 +1,89 @@
+import type { PermissionGrant } from "#shared/types/permission"
+import { Greenhouse } from "~~/server/models/greenhouse"
+import { Permission } from "~~/server/models/permission"
+
+//
+
+/**
+ * - Checks if the permission is already granted.
+ * - Ensures granter is the owner.
+ */
+const grantPermission = async (
+	data: PermissionGrant,
+	granterId: number
+): Promise<SafeResult<Permission>> => {
+	try {
+		// --- Check existing permission
+		const permCount = await Permission.count({ where: { ...data } })
+
+		// --- Already granted
+		if (permCount > 0) {
+			return { success: false, error: "Permission already granted." }
+		}
+
+		// --- Find the greenhouse and check if granter is the owner
+		const gh = await Greenhouse.findOne({
+			where: { id: data.greenhouseId },
+			attributes: ["userId"],
+		})
+
+		// --- Granter isn't the owner
+		if (!gh) return { success: false, error: "Greenhouse not found." }
+		if (gh.userId != granterId) {
+			return {
+				success: false,
+				error: "Only the owner can grant permissions.",
+			}
+		}
+
+		// --- Create and return the permission
+		const perm = await Permission.create({ ...data })
+		return { success: true, data: perm }
+	} catch (error) {
+		console.error(error)
+		return { success: false, error: "Something went wrong." }
+	}
+}
+
+/**
+ * - Checks if the permission exists.
+ * - Ensures revoker is the owner.
+ */
+const revokePermission = async (
+	data: PermissionRevoke,
+	revokerId: number
+): Promise<SafeResult> => {
+	try {
+		// --- Find permssion first
+		const perm = await Permission.findOne({ where: { ...data } })
+
+		// --- Permission doesn't exist
+        if (!perm) return { success: false, error: "Permission isn't granted." }
+        
+        // --- Check first if the revoker is the owner
+        const gh = await Greenhouse.findOne({
+			where: { id: data.greenhouseId },
+			attributes: ["userId"],
+		})
+
+		// --- Granter isn't the owner
+		if (!gh) return { success: false, error: "Greenhouse not found." }
+		if (gh.userId != revokerId) {
+			return {
+				success: false,
+				error: "Only the owner can revoke permissions.",
+			}
+        }
+        
+        // --- Remove permssion
+        await perm.destroy()
+        return { success: true, data: undefined }
+	} catch (error) {
+		console.error(error)
+		return { success: false, error: "Something went wrong." }
+	}
+}
+
+//
+
+export { grantPermission, revokePermission }
