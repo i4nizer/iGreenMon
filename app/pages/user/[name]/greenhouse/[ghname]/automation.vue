a3160636fc9df645f90c9adb0f7a3371e323a3b9
i4nizer <iansandoval264@gmail.com>
2025-08-24 23:16:34 +0800

    Added condition CRUD on automation page and added more permission checks.

--- app/pages/user/[name]/greenhouse/[ghname]/automation.vue
+++ app/pages/user/[name]/greenhouse/[ghname]/automation.vue
@@ -11,7 +11,7 @@
                 <v-dialog class="w-100 w-md-50">
                     <template #activator="{ props: activatorProps }">
                         <v-btn 
-                            v-if="isOwnGH || canCreate(`Threshold`, permissions)" 
+                            v-if="isOwnGH || canCreateThreshold" 
                             class="bg-green" 
                             :="activatorProps"
                         >
@@ -44,8 +44,33 @@
                     />
                 </template>
             </v-dialog>
+            <!-- Condition Create Dialog -->
+			<v-dialog class="w-100 w-md-50" v-model="conditionCreateDialog">
+				<template #default>
+					<condition-create-form
+						class="bg-white rounded"
+						:outputs
+						:threshold-id="conditionCreateThresholdId"
+						@error="(e) => toastUtil.error(e)"
+						@success="onCreateConditionSuccess"
+					/>
+				</template>
+			</v-dialog>
+			<!-- Condition Update Dialog -->
+			<v-dialog class="w-100 w-md-50" v-model="conditionUpdateDialog">
+				<template #default>
+					<condition-update-form
+						class="bg-white rounded"
+						:outputs
+						:condition="(conditionUpdateData as Condition)"
+						@error="(e) => toastUtil.error(e)"
+						@success="onUpdateConditionSuccess"
+					/>
+				</template>
+			</v-dialog>
             <!-- Threshold Lists -->
             <v-col 
+                v-if="isOwnGH || canAccessThreshold"
                 v-for="threshold in thresholds" 
                 xs="12" 
                 sm="6" 
@@ -56,8 +81,6 @@
             >
                 <threshold-card
                     :threshold
-                    :hide-edit="!isOwnGH && !canModify(`Threshold`, permissions)"
-                    :hide-delete="!isOwnGH && !canDelete(`Threshold`, permissions)"
                     @edit="onEditThreshold"
                     @toggle="onToggleThreshold"
                     @delete="onDeleteThreshold"
@@ -65,10 +88,45 @@
                     <template #menu="{ onEdit, onToggle, onDelete }">
                         <threshold-card-menu
                             :threshold
+                            :hide-edit="!isOwnGH && !canModifyThreshold"
+                            :hide-toggle="!isOwnGH && !canModifyThreshold"
+                            :hide-delete="!isOwnGH && !canDeleteThreshold"
                             @edit="onEdit"
                             @toggle="onToggle"
                             @delete="onDelete"
                         ></threshold-card-menu>
+                    </template>
+                    <template #condition>
+                        <v-list>
+                            <v-list-subheader class="d-flex justify-end">
+                                <v-btn 
+                                    v-if="isOwnGH || canCreateCondition"
+                                    text="Add Condtion"
+                                    class="border"
+                                    @click="onCreateCondition(threshold)"
+                                ></v-btn>
+                            </v-list-subheader>
+                            <v-list-item 
+                                v-if="isOwnGH || canAccessCondition"
+                                v-for="condition in conditions.filter((c) => c.thresholdId == threshold.id)"
+                                :key="condition.id"
+                            >
+                                <condition-card 
+                                    :output="(outputs.find((o) => o.id == condition.outputId) as Output)"
+                                    :condition="condition"
+                                    :hide-edit="!isOwnGH && !canModifyCondition"
+                                    :hide-delete="!isOwnGH && !canDeleteCondition"
+                                    @edit="onEditCondition"
+                                    @delete="onDeleteCondition"
+                                />
+                            </v-list-item>
+                            <v-list-item 
+                                v-if="!conditions.some((c) => c.thresholdId == threshold.id)" 
+                                class="text-center"
+                            >
+                                <span class="text-grey">No Condition Yet</span>
+                            </v-list-item>
+                        </v-list>
                     </template>
                 </threshold-card>
             </v-col>
@@ -87,6 +145,8 @@
 <script setup lang="ts">
 import type { Threshold } from '~~/shared/schema/threshold'
 import type { Greenhouse } from '~~/shared/schema/greenhouse'
+import type { Condition } from '~~/shared/schema/condition'
+import type { Output } from '~~/shared/schema/output'
 
 //
 
@@ -136,7 +196,13 @@ const thresholdUtil = useThreshold()
 const thresholdStore = useThresholdStore()
 const { thresholds } = thresholdStore
 
+const canCreateThreshold = computed(() => canCreate("Threshold", permissions))
+const canAccessThreshold = computed(() => canAccess("Threshold", permissions))
+const canModifyThreshold = computed(() => canModify("Threshold", permissions))
+const canDeleteThreshold = computed(() => canDelete("Threshold", permissions))
+
 const fetchThresholds = async () => {
+    if (!isOwnGH && !canAccessThreshold.value) return
     if (!gh.value || thresholds.length > 0) return;
     const res = await thresholdUtil.retrieveAll(gh.value.id)
     if (!res.success) return toastUtil.error(res.error)
@@ -195,12 +261,94 @@ const onDeleteThresholdSuccess = (threshold: Threshold) => {
     toastUtil.success("Threshold deleted successfully.")
 }
 
+// --- Outputs
+const outputUtil = useOutput()
+const outputStore = useOutputStore()
+const { outputs } = outputStore
+
+const canAccessOutput = computed(() => canAccess("Output", permissions))
+
+const fetchOutputs = async () => {
+    if (!isOwnGH && !canAccessOutput.value) return;
+    if (outputs.length > 0) return;
+    const res = await outputUtil.retrieveAllByGH(ghname)
+    if (!res.success) return toastUtil.error(res.error)
+    res.data.forEach((o) => outputStore.append(o))
+}
+
+// --- Conditions
+const conditionUtil = useCondition()
+const conditionStore = useConditionStore()
+const { conditions } = conditionStore
+
+const canCreateCondition = computed(() => canCreate("Condition", permissions))
+const canAccessCondition = computed(() => canAccess("Condition", permissions))
+const canModifyCondition = computed(() => canModify("Condition", permissions))
+const canDeleteCondition = computed(() => canDelete("Condition", permissions))
+
+const fetchConditions = async () => { 
+    if (!isOwnGH && !canAccessCondition.value) return
+    if (conditions.length > 0) return;
+    const res = await conditionUtil.retrieveAllByGH(ghname)
+    if (!res.success) return toastUtil.error(res.error)
+    res.data.forEach((c) => conditionStore.append(c))
+}
+
+// --- Condition CRUD
+const conditionCreateDialog = ref(false)
+const conditionCreateThresholdId = ref(-1)
+const conditionUpdateData = ref<Condition>()
+const conditionUpdateDialog = ref(false)
+
+const onCreateCondition = async (threshold: Threshold) => {
+    conditionCreateThresholdId.value = threshold.id
+    conditionCreateDialog.value = true
+}
+
+const onEditCondition = async (
+    condition: Condition,
+    opts: { loading: Ref<boolean> }
+) => {
+    conditionUpdateData.value = condition
+    conditionUpdateDialog.value = true
+}
+
+const onDeleteCondition = async (
+    condition: Condition,
+    opts: { loading: Ref<boolean> }
+) => {
+    opts.loading.value = true
+    const res = await conditionUtil.destroy(condition.id)
+    opts.loading.value = false
+
+    if (!res.success) toastUtil.error(res.error)
+    else onDeleteConditionSuccess(condition)
+}
+
+const onCreateConditionSuccess = (condition: Condition) => {
+    conditionStore.append(condition)
+    toastUtil.success("Condition created successfully.")
+}
+
+const onUpdateConditionSuccess = (condition: Condition) => {
+    conditionStore.change(condition)
+    toastUtil.success("Condition updated successfully.")
+    conditionUpdateDialog.value = false
+}
+
+const onDeleteConditionSuccess = (condition: Condition) => {
+    conditionStore.remove(condition.id)
+    toastUtil.success("Condition deleted successfully.")
+}
+
 // --- Fetch Data
 const fetchData = async () => {
     await rwnctx(fetchGH)
+    await rwnctx(fetchPerms)
     await Promise.all([
-        rwnctx(fetchPerms),
+        rwnctx(fetchOutputs),
         rwnctx(fetchThresholds),
+        rwnctx(fetchConditions),
     ])
 }
 
