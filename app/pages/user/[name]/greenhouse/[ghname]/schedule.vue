be2e70c982b60249d007df36f2b0e6a12140fe0c
i4nizer <iansandoval264@gmail.com>
2025-08-26 05:50:40 +0800

    Added action components for CRUD in schedule page.

--- app/pages/user/[name]/greenhouse/[ghname]/schedule.vue
+++ app/pages/user/[name]/greenhouse/[ghname]/schedule.vue
@@ -44,6 +44,36 @@
                     />
                 </template>
             </v-dialog>
+            <!-- Action Create Dialog -->
+			<v-dialog class="w-100 w-md-50" v-model="actionCreateDialog">
+				<template #default>
+					<action-create-form
+                        v-if="gh"
+                        hide-schedule
+                        hide-threshold
+						class="bg-white rounded overflow-auto"
+						:inputs
+						:schedule-id="actionCreateScheduleId"
+                        :greenhouse-id="gh.id"
+						@error="(e) => toastUtil.error(e)"
+						@success="onCreateActionSuccess"
+					/>
+				</template>
+			</v-dialog>
+			<!-- Action Update Dialog -->
+			<v-dialog class="w-100 w-md-50" v-model="actionUpdateDialog">
+				<template #default>
+					<action-update-form
+                        hide-schedule
+                        hide-threshold
+                        class="bg-white rounded overflow-auto"
+						:inputs
+						:action="(actionUpdateData as Action)"
+						@error="(e) => toastUtil.error(e)"
+						@success="onUpdateActionSuccess"
+					/>
+				</template>
+			</v-dialog>
             <!-- Schedule Lists -->
             <v-col 
                 v-if="isOwnGH || canAccessSchedule"
@@ -72,6 +102,39 @@
                             @delete="onDelete"
                         ></schedule-card-menu>
                     </template>
+                    <template #action>
+                        <v-list>
+                            <v-list-subheader class="d-flex justify-end">
+                                <v-btn 
+                                    v-if="isOwnGH || canCreateAction"
+                                    text="Add Action"
+                                    class="border"
+                                    @click="onCreateAction(schedule)"
+                                ></v-btn>
+                            </v-list-subheader>
+                            <v-list-item 
+                                v-if="isOwnGH || canAccessAction"
+                                v-for="action in actions.filter((a) => a.scheduleId == schedule.id)"
+                                :key="action.id"
+                            >
+                                <action-card 
+                                    v-if="inputs.some((i) => i.id == action.inputId)"
+                                    :input="(inputs.find((i) => i.id == action.inputId) as Input)"
+                                    :action="action"
+                                    :hide-edit="!isOwnGH && !canModifyAction"
+                                    :hide-delete="!isOwnGH && !canDeleteAction"
+                                    @edit="onEditAction"
+                                    @delete="onDeleteAction"
+                                />
+                            </v-list-item>
+                            <v-list-item 
+                                v-if="!actions.some((c) => c.scheduleId == schedule.id)" 
+                                class="text-center"
+                            >
+                                <span class="text-grey">No Action Yet</span>
+                            </v-list-item>
+                        </v-list>
+                    </template>
                 </schedule-card>
             </v-col>
             <!-- Fallback/emptystate when no schedule -->
@@ -90,6 +153,8 @@
 import type { Schedule } from '~~/shared/schema/schedule'
 import type { Greenhouse } from '~~/shared/schema/greenhouse'
 import type { Condition } from '~~/shared/schema/condition'
+import type { Action } from '~~/shared/schema/action'
+import type { Input } from '~~/shared/schema/input'
 
 //
 
@@ -204,12 +269,95 @@ const onDeleteScheduleSuccess = (schedule: Schedule) => {
     toastUtil.success("Schedule deleted successfully.")
 }
 
+// --- Inputs
+const inputUtil = useInput()
+const inputStore = useInputStore()
+const { inputs } = inputStore
+
+const canAccessInput = computed(() => canAccess("Input", permissions))
+
+const fetchInputs = async () => {
+    if (!isOwnGH && !canAccessInput.value) return;
+    if (inputs.length > 0) return;
+    const res = await inputUtil.retrieveAllByGH(ghname)
+    if (!res.success) return toastUtil.error(res.error)
+    res.data.forEach((o) => inputStore.append(o))
+}
+
+// --- Actions
+const actionUtil = useAction()
+const actionStore = useActionStore()
+const { actions } = actionStore
+
+const canCreateAction = computed(() => canCreate("Action", permissions))
+const canAccessAction = computed(() => canAccess("Action", permissions))
+const canModifyAction = computed(() => canModify("Action", permissions))
+const canDeleteAction = computed(() => canDelete("Action", permissions))
+
+const fetchActions = async () => { 
+    if (!isOwnGH && !canAccessAction.value) return
+    if (!gh.value || actions.length > 0) return;
+    const res = await actionUtil.retrieveAll(gh.value.id)
+    if (!res.success) return toastUtil.error(res.error)
+    res.data.forEach((c) => actionStore.append(c))
+}
+
+// --- Action CRUD
+const actionCreateDialog = ref(false)
+const actionCreateScheduleId = ref(-1)
+const actionUpdateData = ref<Action>()
+const actionUpdateDialog = ref(false)
+
+const onCreateAction = async (schedule: Schedule) => {
+    actionCreateScheduleId.value = schedule.id
+    actionCreateDialog.value = true
+}
+
+const onEditAction = async (
+    action: Action,
+    opts: { loading: Ref<boolean> }
+) => {
+    actionUpdateData.value = action
+    actionUpdateDialog.value = true
+}
+
+const onDeleteAction = async (
+    action: Action,
+    opts: { loading: Ref<boolean> }
+) => {
+    opts.loading.value = true
+    const res = await actionUtil.destroy(action.id)
+    opts.loading.value = false
+
+    if (!res.success) toastUtil.error(res.error)
+    else onDeleteActionSuccess(action)
+}
+
+const onCreateActionSuccess = (action: Action) => {
+    actionStore.append(action)
+    toastUtil.success("Action created successfully.")
+}
+
+const onUpdateActionSuccess = (action: Action) => {
+    actionStore.change(action)
+    toastUtil.success("Action updated successfully.")
+    actionUpdateDialog.value = false
+    actionUpdateData.value = undefined
+}
+
+const onDeleteActionSuccess = (action: Action) => {
+    actionStore.remove(action.id)
+    toastUtil.success("Action deleted successfully.")
+}
+
 // --- Fetch Data
 const fetchData = async () => {
     await rwnctx(fetchGH)
     await rwnctx(fetchPerms)
     await Promise.all([
         rwnctx(fetchSchedules),
+        rwnctx(fetchInputs),
+        rwnctx(fetchActions),
     ])
 }
 
