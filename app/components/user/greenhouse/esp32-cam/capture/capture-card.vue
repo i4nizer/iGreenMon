111de3f8a1d1e29a1576a3bf9e95c78bcfdb34e8
i4nizer <iansandoval264@gmail.com>
2025-08-28 17:28:45 +0800

    Created capture card and image canvas component for rendering images.

---
+++ app/components/user/greenhouse/esp32-cam/capture/capture-card.vue
@@ -0,0 +1,127 @@
+<template>
+    <v-card class="border" :loading>
+        <v-card-item class="pa-0">
+            <slot 
+                name="canvas" 
+                :="{ capture, onImageLoad, onImageDraw, onImageClick }"
+            />
+        </v-card-item>
+        <v-card-subtitle class="text-wrap">
+            <span>{{ capture.filename }}</span>
+        </v-card-subtitle>
+        <v-card-subtitle class="text-wrap">
+            <span class="text-caption">
+                {{ useDate().format(capture.createdAt, "fullDateTime12h") }}
+            </span>
+        </v-card-subtitle>
+        <v-card-actions class="text-wrap overflow-wrap">
+            <v-btn
+                size="small"
+                icon="mdi-image"
+                class="text-green-darken-3 elevation-0"
+                v-tooltip="`Download Image`"
+                :disabled="!image"
+                @click="onClickRaw"
+            ></v-btn>
+            <v-btn
+                size="small"
+                icon="mdi-download"
+                class="text-green-darken-3 elevation-0"
+                v-tooltip="`Download Image with Bounding Boxes`"
+                :disabled="!canvas"
+                @click="onClickSave"
+            ></v-btn>
+            <v-btn
+                size="small"
+                icon="mdi-image-search"
+                class="text-green-darken-3 elevation-0"
+                v-tooltip="`View Image with Bounding Boxes`"
+                :disabled="!canvas"
+                @click="onClickView"
+            ></v-btn>
+        </v-card-actions>
+    </v-card>
+</template>
+
+<script setup lang="ts">
+import type { Capture } from '~~/shared/schema/capture';
+
+//
+
+// --- Data Binding
+const props = defineProps<{ capture: Capture }>()
+
+// --- State Binding
+const emit = defineEmits<{
+    raw: [
+        capture: Capture,
+        image: HTMLImageElement,
+        opts: { loading: Ref<boolean> },
+    ]
+    save: [
+        capture: Capture,
+        canvas: HTMLCanvasElement,
+        opts: { loading: Ref<boolean> },
+    ]
+    view: [
+        capture: Capture,
+        canvas: HTMLCanvasElement,
+        opts: { loading: Ref<boolean> },
+    ]
+    load: [
+        capture: Capture,
+        image: HTMLImageElement,
+        opts: { loading: Ref<boolean> },
+    ]
+    draw: [
+        capture: Capture,
+        canvas: HTMLCanvasElement,
+        opts: { loading: Ref<boolean> },
+    ]
+}>()
+
+// --- Local Actions
+const loading = ref(true)
+
+const onClickRaw = () => {
+    const img = image.value as HTMLImageElement
+    emit("raw", props.capture, img, { loading })
+}
+
+const onClickSave = () => {
+    const cvs = canvas.value as HTMLCanvasElement
+    emit("save", props.capture, cvs, { loading })
+}
+
+const onClickView = () => {
+    const cvs = canvas.value as HTMLCanvasElement
+    emit("view", props.capture, cvs, { loading })
+}
+
+// --- Child Actions
+const image = ref<HTMLImageElement>()
+const canvas = ref<HTMLCanvasElement>()
+
+const onImageLoad = (el: HTMLImageElement) => {
+    image.value = el
+    emit("load", props.capture, el, { loading })
+}
+
+const onImageDraw = (el: HTMLCanvasElement) => {
+    canvas.value = el
+    loading.value = false
+    emit("draw", props.capture, el, { loading })
+}
+
+const onImageClick = (el: HTMLCanvasElement) => {
+    canvas.value = el
+    emit("view", props.capture, el, { loading })
+}
+
+//
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ app/components/user/greenhouse/esp32-cam/capture/image-canvas.vue
@@ -0,0 +1,130 @@
+<template>
+    <div class="text-center text-wrap overflow-wrap">
+        <canvas
+            ref="canvasRef"
+            class="cursor-pointer w-100 aspect-ratio-1"
+            v-tooltip="`Click to View`"
+            :width="canvasSize.width"
+            :height="canvasSize.height"
+            @click="emit(`click`, canvasRef as HTMLCanvasElement)"
+        ></canvas>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+//
+
+// --- Data Binding
+const emit = defineEmits<{
+    "load": [image: HTMLImageElement]
+    "draw": [canvas: HTMLCanvasElement]
+    "click": [canvas: HTMLCanvasElement]
+}>()
+
+const props = defineProps<{ src: string }>()
+
+// --- Template Binding
+const imageRef = ref<HTMLImageElement>()
+const canvasRef = ref<HTMLCanvasElement>()
+const canvasCtx = ref<CanvasRenderingContext2D>()
+const canvasSize = reactive({ width: 4000, height: 4000 })
+
+// --- Resizing & Visibility Observing
+const state = reactive({
+    visible: true,
+    painted: false,
+    resizing: false,
+})
+
+const sizeObserverUtil = useSizeObserver()
+const intersectionObserver = ref<IntersectionObserver>()
+
+watch(
+    () => props,
+    (nv, ov) => {
+        state.painted = nv.src != ov.src
+        if (imageRef.value) imageRef.value.src = nv.src
+    },
+    { deep: true }
+)
+
+// --- Image Rendering
+const render = () => {
+    // --- Check states & existence
+    if (state.painted || state.resizing || !state.visible) return
+    if (!imageRef.value || !canvasRef.value || !canvasCtx.value) return
+
+    // --- Calc aspect ratio to center image
+    const { naturalWidth: imgW, naturalHeight: imgH } = imageRef.value
+    const side = Math.min(imgW, imgH)
+    const sideX = (imgW - side) / 2
+    const sideY = (imgH - side) / 2
+
+    // --- Draw image first
+    const { width, height } = canvasRef.value
+    canvasCtx.value.imageSmoothingEnabled = false
+    canvasCtx.value.drawImage(imageRef.value, sideX, sideY, side, side, 0, 0, width, height)
+
+    // --- Emit drawn image in canvas
+    emit("draw", canvasRef.value)
+    state.painted = true
+}
+
+// --- Event Handling
+const onImageLoad = (event: Event) => {
+    emit("load", event.target as HTMLImageElement)
+    state.visible = true
+    requestAnimationFrame(render)
+}
+
+const onIntersect: IntersectionObserverCallback = (
+    entries: IntersectionObserverEntry[]
+) => {
+    for (const e of entries) {
+        state.visible = e.isIntersecting
+        if (e.isIntersecting) render()
+        else state.painted = false
+    }
+}
+
+const onResizeCanvas = (el: HTMLElement, w: number, h: number) => {
+    state.painted = false
+
+    state.resizing = true
+    canvasSize.width = w
+    canvasSize.height = h
+    state.resizing = false
+
+    render()
+}
+
+// --- Binding Data on LifeCycle
+onMounted(() => {
+    imageRef.value = new Image()
+    imageRef.value.onload = onImageLoad
+    imageRef.value.src = props.src
+
+    if (!canvasRef.value) return
+    canvasCtx.value = canvasRef.value.getContext('2d') ?? undefined
+    sizeObserverUtil.observe(canvasRef.value, 250, onResizeCanvas)
+
+    intersectionObserver.value = new IntersectionObserver(
+        onIntersect,
+        { threshold: 0.1 }
+    )
+    intersectionObserver.value.observe(canvasRef.value)
+})
+
+onUnmounted(() => {
+    sizeObserverUtil.stop()
+    intersectionObserver.value?.disconnect()
+})
+
+//
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
