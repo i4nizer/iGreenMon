a1a77c3993fc20533c4a9311ae16dc1769926e62
i4nizer <iansandoval264@gmail.com>
2025-08-31 15:35:38 +0800

    Added background color too for npk model class colors.

--- shared/types/model/npk.ts
+++ shared/types/model/npk.ts
@@ -10,10 +10,10 @@ const NPKModelClass = [
 type NPKModelClass = (typeof NPKModelClass)[number]
 
 const NPKModelClassColor = [
-	{ class: NPKModelClass[0], color: "#4CAF50" },
-	{ class: NPKModelClass[1], color: "#FFEB3B" },
-	{ class: NPKModelClass[2], color: "#673AB7" },
-	{ class: NPKModelClass[3], color: "#795548" },
+	{ class: NPKModelClass[0], color: "#4CAF50", background: "#1B5E20" },
+	{ class: NPKModelClass[1], color: "#FFEB3B", background: "#212121" },
+	{ class: NPKModelClass[2], color: "#673AB7", background: "#D1C4E9" },
+	{ class: NPKModelClass[3], color: "#795548", background: "#EFEBE9" },
 ] as const
 type NPKModelClassColor = (typeof NPKModelClassColor)[number]
 
