b9523793eb253aa23c692dc00cafd563c712c51a
i4nizer <iansandoval264@gmail.com>
2025-09-02 01:35:28 +0800

    Applied pagination on reading composable.

--- app/composables/data/use-reading.ts
+++ app/composables/data/use-reading.ts
@@ -4,48 +4,72 @@ import type { Reading } from "~~/shared/schema/reading"
 
 export const useReading = () => {
     // --- Main CRUD functions
-    const retrieveAllByOutput = async(
-        outputid: number
-    ): Promise<SafeResult<Reading[]>> => {
-        try {
-            const reqFetch = useRequestFetch()
-            const url = `/api/user/greenhouse/esp32/sensor/output/reading`
-            const query = `?outputid${outputid}`
-            const res = await reqFetch<Reading[]>(url + query)
-            return { success: true, data: res }
-        } catch (error) {
-            const msg = (error as any)?.statusMessage ?? "Something went wrong."
-            return { success: false, error: msg }
-        }
-    }
+    const retrieveAllByOutput = async (
+		outputid: number,
+		alpha?: Date,
+		omega?: Date,
+		limit?: number,
+		offset?: number
+	): Promise<SafeResult<Reading[]>> => {
+		try {
+			const reqFetch = useRequestFetch()
+            let url = `/api/user/greenhouse/esp32/sensor/output/reading`
+			url += `?outputid${outputid}`
+            if (alpha) url += `&alpha=${alpha}`
+			if (omega) url += `&omega=${omega}`
+			if (limit) url += `&limit=${limit}`
+			if (offset) url += `&offset=${offset}`
+			const res = await reqFetch<Reading[]>(url)
+			return { success: true, data: res }
+		} catch (error) {
+			const msg = (error as any)?.statusMessage ?? "Something went wrong."
+			return { success: false, error: msg }
+		}
+	}
     
     const retrieveAllByEsp32 = async (
-        esp32id: number
-    ): Promise<SafeResult<Reading[]>> => {
-        try {
-            const reqFetch = useRequestFetch()
-            const url = `/api/user/greenhouse/esp32/reading?esp32id=${esp32id}`
-            const res = await reqFetch<Reading[]>(url)
-            return { success: true, data: res }
-        } catch (error) {
-            const msg = (error as any)?.statusMessage ?? "Something went wrong."
-            return { success: false, error: msg }
-        }
-    }
+		esp32id: number,
+		alpha?: Date,
+		omega?: Date,
+		limit?: number,
+		offset?: number
+	): Promise<SafeResult<Reading[]>> => {
+		try {
+			const reqFetch = useRequestFetch()
+            let url = `/api/user/greenhouse/esp32/reading?esp32id=${esp32id}`
+            if (alpha) url += `&alpha=${alpha}`
+			if (omega) url += `&omega=${omega}`
+			if (limit) url += `&limit=${limit}`
+			if (offset) url += `&offset=${offset}`
+			const res = await reqFetch<Reading[]>(url)
+			return { success: true, data: res }
+		} catch (error) {
+			const msg = (error as any)?.statusMessage ?? "Something went wrong."
+			return { success: false, error: msg }
+		}
+	}
     
     const retrieveAllByGH = async (
-        ghname: string
-    ): Promise<SafeResult<Reading[]>> => {
-        try {
-            const reqFetch = useRequestFetch()
-            const url = `/api/user/greenhouse/reading?ghname=${ghname}`
-            const res = await reqFetch<Reading[]>(url)
-            return { success: true, data: res }
-        } catch (error) {
-            const msg = (error as any)?.statusMessage ?? "Something went wrong."
-            return { success: false, error: msg }
-        }
-    }
+		ghname: string,
+		alpha?: Date,
+		omega?: Date,
+		limit?: number,
+		offset?: number
+	): Promise<SafeResult<Reading[]>> => {
+		try {
+			const reqFetch = useRequestFetch()
+            let url = `/api/user/greenhouse/reading?ghname=${ghname}`
+            if (alpha) url += `&alpha=${alpha}`
+			if (omega) url += `&omega=${omega}`
+			if (limit) url += `&limit=${limit}`
+			if (offset) url += `&offset=${offset}`
+			const res = await reqFetch<Reading[]>(url)
+			return { success: true, data: res }
+		} catch (error) {
+			const msg = (error as any)?.statusMessage ?? "Something went wrong."
+			return { success: false, error: msg }
+		}
+	}
 
     // --- Expose
     return {
