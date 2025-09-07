43a3d65ddd380ebb97cc42d4184938afd808f4f7
i4nizer <iansandoval264@gmail.com>
2025-09-08 00:12:06 +0800

    Implemented the api key on settings page of esp32.

--- app/pages/user/[name]/greenhouse/[ghname]/esp32/[esp32id]/[esp32name]/settings.vue
+++ app/pages/user/[name]/greenhouse/[ghname]/esp32/[esp32id]/[esp32name]/settings.vue
@@ -6,6 +6,21 @@
             </v-col>
         </v-row>
         <v-row>
+            <v-col cols="12">
+                <v-card>
+                    <v-card-text>
+                        <v-text-field
+                            readonly
+                            hide-details
+                            label="Api Key"
+                            append-icon="mdi-clipboard-outline"
+                            prepend-inner-icon="mdi-key-outline"
+                            v-model="apiKey"
+                            @click:append="onClickCopyApiKey"
+                        ></v-text-field>
+                    </v-card-text>
+                </v-card>
+            </v-col>
             <v-col cols="12" md="6" lg="4" xl="3">
                 <v-card class="pa-5">
                     <esp32-settings-form
@@ -53,8 +68,28 @@ const onEsp32UpdateSuccess = (e: Esp32) => {
     toastUtil.success("Esp32 updated successfully.")
 }
 
+// --- Esp32 Api Key
+const apiKey = useState<string>(`${esp32id}-api-key`, () => "")
+
+const onClickCopyApiKey = async () => {
+    await navigator.clipboard.writeText(apiKey.value)
+        .then(() => toastUtil.success(`Api key copied to clipboard.`))
+        .catch((e) => toastUtil.error(e))
+}
+
+const fetchApiKey = async () => {
+    if (apiKey.value) return
+    const esp32Id = parseInt(esp32id)
+    const res = await esp32Util.retrieveKey(esp32Id)
+    if (!res.success) return toastUtil.error(res.error)
+    apiKey.value = res.data
+}
+
 // --- Data Fetching
-const fetchData = async () => await rwnctx(fetchEsp32)
+const fetchData = async () => {
+    await rwnctx(fetchEsp32)
+    await rwnctx(fetchApiKey)
+}
 
 onBeforeMount(fetchData)
 onServerPrefetch(fetchData)
