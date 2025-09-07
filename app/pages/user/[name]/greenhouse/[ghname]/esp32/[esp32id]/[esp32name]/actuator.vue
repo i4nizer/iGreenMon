c5afdf3478e5dd8dd0a568add82d60fc986853a6
i4nizer <iansandoval264@gmail.com>
2025-09-07 22:40:22 +0800

    Fixed incorrect esp32-id in actuator create form.

--- app/pages/user/[name]/greenhouse/[ghname]/esp32/[esp32id]/[esp32name]/actuator.vue
+++ app/pages/user/[name]/greenhouse/[ghname]/esp32/[esp32id]/[esp32name]/actuator.vue
@@ -23,7 +23,7 @@
 					</template>
 					<template #default>
 						<actuator-create-form
-							:esp32-id="gh?.id ?? -1"
+							:esp32-id="parseInt(esp32id)"
 							class="bg-white rounded"
 							@error="(e) => toastUtil.error(e)"
 							@success="onCreateActuatorSuccess"
