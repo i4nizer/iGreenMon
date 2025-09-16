5abc3f1a599971419a303b897bec27f6465f3690
i4nizer <iansandoval264@gmail.com>
2025-09-16 19:25:00 +0800

    Modified cloudflare tunnel url emailing to all users including the owner.

--- server/services/adapter/cloudflared-email/url.ts
+++ server/services/adapter/cloudflared-email/url.ts
@@ -1,32 +1,41 @@
 import cloudflared from "../../cloudflared"
+import { User } from "~~/server/models/user"
 import { queueEmail } from "../../email"
 
 //
 
 const onCloudflaredUrl = async (url: string) => {
 	console.info(`Cloudflared tunnel active on ${url}.`)
-    
-    const ssres = await useTemplate({
+
+	const ssres = await useTemplate({
 		type: "Cloudflared-Tunnel",
 		safe: true,
 		data: { url },
-    })
-    
-    if (!ssres.success) return console.error("Cloudflared tunnel email failed.")
-    const config = useRuntimeConfig()
-    
-    const callback = (e: any, i: any) => {
-        if (!!e) return console.error("Cloudflared tunnel email failed.")
-        console.info("Cloudflared tunnel url emailed.")
-    }
+	})
+	if (!ssres.success) return console.error("Cloudflared tunnel email failed.")
 
-    queueEmail(
-        config.gmailAddress,
-        "Cloudflared Tunnel",
-        undefined,
-        ssres.data,
-        callback
-    )
+	// --- Email to owner
+	const config = useRuntimeConfig()
+	const callback = (e: any, i: any) => {
+		if (!!e) return console.error("Cloudflared tunnel email failed.")
+		console.info("Cloudflared tunnel url emailed.")
+	}
+
+    const gmail = config.gmailAddress
+	queueEmail(gmail, "Cloudflared Tunnel", undefined, ssres.data, callback)
+
+	// --- Email to users
+	const users = await User.findAll({ attributes: ["email"] })
+	for (const user of users) {
+		const email = user.email
+
+		const callback = (e: any, i: any) => {
+			if (!!e) return console.error("Cloudflared tunnel email failed.")
+			console.info(`Cloudflared tunnel url emailed to ${email}.`)
+		}
+
+		queueEmail(email, "Cloudflared Tunnel", undefined, ssres.data, callback)
+	}
 }
 
 //
