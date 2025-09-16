b7df3db5133b5c22a7cc03fc54495cc6fe1731ba
i4nizer <iansandoval264@gmail.com>
2025-09-16 23:06:06 +0800

    Increased delay on sharing cloudflare url from 5sec to 60sec.

--- server/plugins/cloudflared.ts
+++ server/plugins/cloudflared.ts
@@ -5,7 +5,7 @@ import cloudflared from "../services/cloudflared"
 
 export default defineNitroPlugin(async () => {
     // --- Init cloudflare tunnel
-    await setTimeout(5000)
+    await setTimeout(60000)
     await cloudflared.init("http://127.0.0.1:3000")
     console.info("Cloudflared service initialized.")
 })
