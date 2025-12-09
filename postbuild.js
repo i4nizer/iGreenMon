d5c48c8512522cb51554e3cb32bf2b78ac7b28dd
i4nizer <iansandoval264@gmail.com>
2025-12-10 07:06:13 +0800

    Added postbuild script to copy left binaries.

---
+++ postbuild.js
@@ -0,0 +1,34 @@
+import fs from "fs/promises"
+import path from "path"
+
+//
+
+/** Utility to copy files recursively. */
+const copy = async (from, to) => {
+    await fs.mkdir(to, { recursive: true })
+    const dir = await fs.readdir(from)
+
+	for (const file of dir) {
+		const sfile = path.join(from, file)
+		const dfile = path.join(to, file)
+		const stat = await fs.stat(sfile)
+
+		if (stat.isDirectory()) await copy(sfile, dfile)
+		else await fs.copyFile(sfile, dfile)
+	}
+
+	console.info(`${from}\n\t=> ${to}.`)
+}
+
+// --- Copy @tensorflow/tfjs-node
+const cwd = process.cwd()
+const tsrc = `${cwd}/node_modules/@tensorflow/tfjs-node`
+const tdest = `${cwd}.output/server/node_modules/@tensorflow/tfjs-node`
+await copy(tsrc, tdest)
+
+// --- Copy cloudflared/bin
+const csrc = `${cwd}/node_modules/cloudflared/bin`
+const cdest = `${cwd}.output/server/node_modules/cloudflared/bin`
+await copy(csrc, cdest)
+
+//
