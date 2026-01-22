60dca089fdb5be02dc85b4dc8c1658dcf319f06f
i4nizer <iansandoval264@gmail.com>
2026-01-22 21:38:21 +0800

    Installed mdi icons to show icons even without internet.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 export default defineNuxtConfig({
 	compatibilityDate: "2025-07-15",
+	css: ["@mdi/font/css/materialdesignicons.css"],
 	devtools: { enabled: false },
 	$production: { ssr: false },
 	nitro: {
