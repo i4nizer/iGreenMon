a127f1d62991d7337d4d937d0299f8d06d278188
i4nizer <iansandoval264@gmail.com>
2026-05-12 06:32:41 +0800

    Fixed infinite log re-queue.

--- server/services/sms/bridger.ts
+++ server/services/sms/bridger.ts
@@ -1,5 +1,6 @@
 import log from "./log";
 import event from "./event";
+import registry from "./registry";
 import { Log } from "~~/shared/schema/log";
 import { WebSocketEventHandler } from "./schema";
 
@@ -11,6 +12,8 @@ const onUpdateLog: WebSocketEventHandler<Log> = (peer, data, user) => {
         for (const [uid, lid] of log.pool.logs) {
             if (lid != l.id) continue
             log.pool.dequeue(uid)
+            const lset = registry.logs.get(uid)
+            if (lset) lset.delete(l.id)
         }
     }
 }
